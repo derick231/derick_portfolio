82c73115639f6c618b30117bcb5850b931e15194
derick <salvationderick23@gmail.com>
2026-02-03 22:06:00 +0545

    added email sending functionality

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,33 @@
 "use client";
 import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactPage = () => {
   const text = "Say Hello ";
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false)
+    setSuccess(false)
+
+    emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, {
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    }).then(
+        () => {
+          setSuccess(true)
+          form.current.reset()
+        },
+        (error) => {
+          setError(false)
+        },
+      );
+  };
+
   return (
     <motion.div
       className="h-full overflow-hidden"
@@ -12,7 +37,7 @@ const ContactPage = () => {
     >
       <div className="h-full px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col lg:flex-row">
         {/* Text Container */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/3 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
@@ -32,13 +57,34 @@ const ContactPage = () => {
           <span>😊</span>
         </div>
         {/* Form Container */}
-        <form className="h-1/2 lg:h-full lg:w-1/2 flex flex-col justify-center gap-8 p-24 bg-red-50 rounded-xl text-xl">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="h-2/3 lg:h-full lg:w-1/2 flex flex-col justify-center gap-4 lg:gap-8 p-24 bg-red-50 rounded-xl text-xl"
+        >
           <span>Dear Derick,</span>
-          <textarea name="" id="" rows={6} className="bg-transparent border-b-2 border-black resize-none outline-none" />
+          <textarea
+            name="user_message"
+            id=""
+            rows={6}
+            className="bg-transparent border-b-2 border-black resize-none outline-none"
+          />
           <span>My Email Address is:</span>
-          <input type="email" className="border-b-2 border-black bg-transparent outline-none"/>
+          <input
+            type="email"
+            name="user_email"
+            className="border-b-2 border-black bg-transparent outline-none"
+          />
           <span>Regards</span>
-          <button className=" bg-purple-200 px-4 py-3 rounded-full shadow-lg">Send</button>
+          <button className=" bg-purple-200 px-4 py-3 rounded-full shadow-lg text-gray-600 font-semibold">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600">Message Sent Successfully!</span>
+          )}
+          {error && (
+            <span className="text-red-600">Error sending message</span>
+          )}
         </form>
       </div>
     </motion.div>
