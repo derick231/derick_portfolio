f3231e224f4dd3df05158d3357b3b336886fec23
derick <salvationderick23@gmail.com>
2026-02-03 17:01:00 +0545

    text animations for contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,15 +1,41 @@
-"use client"
-import {motion} from "framer-motion"
+"use client";
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Say Hello ";
   return (
-     <motion.div
+    <motion.div
       className="h-full overflow-hidden"
-      initial={{ y: "-200vh"}}
+      initial={{ y: "-200vh" }}
       animate={{ y: 0 }}
       transition={{ duration: 1, ease: "easeOut" }}
-    ></motion.div>
-  )
-}
+    >
+      <div className="h-full px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+        {/* Text Container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </div>
+           <span>😊</span>
+        </div>
+        {/* Form Container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,12 +4,13 @@ import { useTransform } from "framer-motion";
 import { useScroll } from "framer-motion";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import { useContext, useRef } from "react";
 
 const items = [
   {
     id: 1,
-    color: "from-red-300 to-blue-300",
+    color: "from-red-800 to-blue-800",
     title: "React Commerce",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
@@ -17,7 +18,7 @@ const items = [
   },
   {
     id: 2,
-    color: "from-blue-300 to-violet-300",
+    color: "from-blue-800 to-gray-800",
     title: "Next.js Medium Blog",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
@@ -25,7 +26,7 @@ const items = [
   },
   {
     id: 3,
-    color: "from-violet-300 to-purple-300",
+    color: "from-gray-800 to-purple-800",
     title: "Vanilla Book App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
@@ -33,7 +34,7 @@ const items = [
   },
   {
     id: 4,
-    color: "from-purple-300 to-red-300",
+    color: "from-purple-800 to-red-800",
     title: "Spotify Music App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -60,6 +61,24 @@ const PortfolioPage = () => {
       animate={{ y: 0 }}
       transition={{ duration: 1, ease: "easeOut" }}
     >
+      <div className="flex justify-end ">
+        <div className="fixed top-96 right-5 lg:right-10 bg-white z-50 shadow-lg w-10 h-10 lg:w-20 lg:h-20 rounded-full flex items-center justify-center">
+          <Link href="/portfolio">
+            
+              <motion.svg
+                animate={{y:"-15px"}}
+                transition={{duration:1.5, ease:"easeInOut", repeat: Infinity, repeatType:"reverse"}}
+                fill="#000000"
+                className="w-4 h-4 lg:w-[30px] lg:h-[30px]"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M11.293,1.293a1,1,0,0,1,1.414,0l5,5a1,1,0,0,1-1.414,1.414L13,4.414V22a1,1,0,0,1-2,0V4.414L7.707,7.707A1,1,0,0,1,6.293,6.293Z" />
+              </motion.svg>
+            
+          </Link>
+        </div>
+      </div>
       <div className="h-[600vh] relative" ref={ref}>
         <div className=" h-[calc(100vh-6rem)] w-screen flex flex-col items-center justify-center">
           <motion.div
@@ -186,18 +205,22 @@ const PortfolioPage = () => {
         </div>
         <div className=" sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex" ref={containerRef}>
-            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-800 to-red-800" />
             {items.map((item) => (
               <div
                 key={item.id}
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
               >
                 <div className="flex flex-col text-white gap-8">
-                  <h1>{item.title}</h1>
-                  <div className="relative">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative  w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                     <Image src={item.img} alt={item.title} fill />
                   </div>
-                  <p>{item.desc}</p>
+                  <p className="w-80 md:w-96 lg:w-[500px] xl:w-[600px] lg:text-lg">
+                    {item.desc}
+                  </p>
                 </div>
               </div>
             ))}
